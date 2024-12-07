9203048310428313ffb9534f474d06dbf5093c0e
konstantin <konst.gromov@gmail.com>
2024-12-07 02:37:17 +0200

    Find way around to use graphql tests

--- src/test/java/com/graphql/RecipeControllerTest.java
+++ src/test/java/com/graphql/RecipeControllerTest.java
@@ -4,13 +4,20 @@ import com.graphql.controllers.RecipeController;
 import com.graphql.domain.Recipe;
 import com.graphql.model.client.RecipeGraphQLQuery;
 import com.graphql.model.client.RecipeProjectionRoot;
+import com.graphql.repositories.CategoryRepository;
+import com.graphql.repositories.RecipeRepository;
 import com.netflix.graphql.dgs.client.codegen.GraphQLQueryRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
+import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureGraphQlTester;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.graphql.test.tester.GraphQlTester;
 
-@GraphQlTest(RecipeController.class)
+// @GraphQlTest is layered test - so only controllers and fetchers are autowired
+//@GraphQlTest(RecipeController.class)
+@SpringBootTest
+@AutoConfigureGraphQlTester
 public class RecipeControllerTest {
 
     @Autowired
@@ -19,11 +26,14 @@ public class RecipeControllerTest {
     @Test
     void shouldGetFirstAccount() {
         var queryRequest = new GraphQLQueryRequest(
-                RecipeGraphQLQuery.newRequest().build(),
+                RecipeGraphQLQuery.newRequest().id("1").build(),
                 new RecipeProjectionRoot().id().notes().category()
         );
         var recipeById = this.graphQlTester
                 .document(queryRequest.serialize())
-                .execute().path("recipe").entity(Recipe.class).get();
+                .execute()
+                .path("data").path("recipe")
+                .entity(Recipe.class)
+                .get();
     }
 }
